b130f11254127906c38b5da46b519b2a045ee272
Loco <Loco>
2016-05-03 23:18:06 +0100

    Protocol & noblks adjusted
    Protocol & noblks adjusted upwards

--- src/version.h
+++ src/version.h
@@ -35,25 +35,25 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70002;
+static const int PROTOCOL_VERSION = 70013;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70002;
+static const int MIN_PEER_PROTO_VERSION = 70013;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 70002;
+static const int MIN_INSTANTX_PROTO_VERSION = 70013;
 
-static const int MIN_MN_PROTO_VERSION = 70002;
+static const int MIN_MN_PROTO_VERSION = 70013;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 60020;
-static const int NOBLKS_VERSION_END = 61401;
+static const int NOBLKS_VERSION_START = 0;
+static const int NOBLKS_VERSION_END = 61440;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
